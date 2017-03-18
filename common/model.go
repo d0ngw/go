e25a87d97de3ec2ad537de2bda2059e00b1eeb17
d0ngw <agile.java@gmail.com>
2017-03-19 02:06:43 +0800

    add ResultSet

--- common/model.go
+++ common/model.go
@@ -2,8 +2,8 @@ package common
 
 //PageParam 分页参数
 type PageParam struct {
-	Page     int
-	PageSize int
+	Page     int `json:"page"`
+	PageSize int `json:"page_size"`
 }
 
 //Limit 根据maxPage和maxPageSize限制Page和PageSize
@@ -32,11 +32,27 @@ func (p *PageParam) EndIndex() int {
 	return p.StartIndex() + p.PageSize - 1
 }
 
+// ResultSet is the result set with total and items
+type ResultSet interface {
+	SetTotal(total int64)
+	SetItems(items []interface{})
+}
+
 //PageResult 分页结果
 type PageResult struct {
 	PageParam
-	Total int64
-	Items interface{}
+	Total int64 `json:"total"`
+	Items []interface{}
+}
+
+// SetTotal implements ResultSet.SetTotal
+func (p *PageResult) SetTotal(total int64) {
+	p.Total = total
+}
+
+// SetItems  implements ResultSet.SetItems
+func (p *PageResult) SetItems(items []interface{}) {
+	p.Items = items
 }
 
 //Query 基本的查询参数
