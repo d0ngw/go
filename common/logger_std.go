daa3868f0e56ae2ccd05e14f53c6f178e2e944e1
d0ngw <agile.java@gmail.com>
2018-08-23 16:15:57 +0800

    add log level

--- common/logger_std.go
+++ common/logger_std.go
@@ -2,39 +2,70 @@ package common
 
 import (
 	"log"
+
+	"go.uber.org/zap"
+
+	"go.uber.org/zap/zapcore"
 )
 
 // StdLogger 使用标准库封装的logger
 type StdLogger struct {
+	logEnable zapcore.LevelEnabler
+}
+
+// NewStdLogger new info level logger
+func NewStdLogger() *StdLogger {
+	logger := &StdLogger{
+		logEnable: zap.InfoLevel,
+	}
+	return logger
 }
 
 // Tracef trace
 func (l *StdLogger) Tracef(format string, params ...interface{}) {
+	if !l.logEnable.Enabled(zap.DebugLevel) {
+		return
+	}
 	log.Printf(format, params...)
 }
 
 // Debugf debug
 func (l *StdLogger) Debugf(format string, params ...interface{}) {
+	if !l.logEnable.Enabled(zap.DebugLevel) {
+		return
+	}
 	log.Printf(format, params...)
 }
 
 // Infof info
 func (l *StdLogger) Infof(format string, params ...interface{}) {
+	if !l.logEnable.Enabled(zap.InfoLevel) {
+		return
+	}
 	log.Printf(format, params...)
 }
 
 // Warnf warn
 func (l *StdLogger) Warnf(format string, params ...interface{}) {
+	if !l.logEnable.Enabled(zap.WarnLevel) {
+		return
+	}
 	log.Printf(format, params...)
 }
 
 // Errorf error
 func (l *StdLogger) Errorf(format string, params ...interface{}) {
+	if !l.logEnable.Enabled(zap.ErrorLevel) {
+		return
+	}
 	log.Printf(format, params...)
 }
 
 // Criticalf critical
 func (l *StdLogger) Criticalf(format string, params ...interface{}) {
+	if !l.logEnable.Enabled(zap.ErrorLevel) {
+		return
+	}
 	log.Printf(format, params...)
 }
 
